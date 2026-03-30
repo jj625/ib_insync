a4d3b13cc3a21b96123c9f31dd93c6504cca6522
jj625 <jjusuf@gmail.com>
2026-03-30 14:34:43 -0400

    linting

--- ib_insync/ib.py
+++ ib_insync/ib.py
@@ -562,7 +562,7 @@ class IB:
 
     def tickersByLocalsymbol(self) -> dict[str, Ticker]:
         """Get a list of all tickers."""
-        return self.wrapper.tickersByLocalsymbol
+        return getattr(self.wrapper, 'tickersByLocalsymbol', {})
     
     def lastTime(self) -> datetime.datetime:
         """ UTC time of last network packet arrival. """
