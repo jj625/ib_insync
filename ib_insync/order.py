722165ad3bc37645dc1aa2ac5ba7b0dc49aa0477
jj625 <jjusuf@gmail.com>
2026-04-28 11:00:14 -0400

    repr for OrderState

--- ib_insync/order.py
+++ ib_insync/order.py
@@ -275,6 +275,12 @@ class OrderState:
     completedTime: str = ''
     completedStatus: str = ''
 
+    def __repr__(self):
+        attrs = dataclassNonDefaults(self)
+        clsName = self.__class__.__qualname__
+        s = ', '.join(f'{k}={v!r}' for k, v in attrs.items())
+        return f'{clsName}({s})'
+    __str__ = __repr__
 
 @dataclass
 class OrderComboLeg:
